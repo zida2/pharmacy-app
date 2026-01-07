176022e78c16fad4550f0d9bb0f210176ae593e0
zida desire <zidedesire20@gmail.com>
2026-01-07 12:59:15 +0000

    Allow all cartocdn subdomains in CSP with wildcard

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://tiles.openfreemap.org https://unpkg.com https://cdn.jsdelivr.net https://apis.google.com; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://tiles.openfreemap.org https://fonts.googleapis.com; img-src 'self' blob: data: https://tiles.openfreemap.org https://api.mapbox.com https://tiles.basemaps.cartocdn.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://*.firestore.googleapis.com https://*.firebaseio.com https://*.googleapis.com https://tiles.openfreemap.org https://router.project-osrm.org https://basemaps.cartocdn.com https://tiles.basemaps.cartocdn.com https://overpass-api.de; worker-src 'self' blob:; frame-src 'self' https://*.firebaseapp.com https://*.google.com;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://tiles.openfreemap.org https://unpkg.com https://cdn.jsdelivr.net https://apis.google.com; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://tiles.openfreemap.org https://fonts.googleapis.com; img-src 'self' blob: data: https://tiles.openfreemap.org https://api.mapbox.com https://*.basemaps.cartocdn.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://*.firestore.googleapis.com https://*.firebaseio.com https://*.googleapis.com https://tiles.openfreemap.org https://router.project-osrm.org https://*.basemaps.cartocdn.com https://overpass-api.de; worker-src 'self' blob:; frame-src 'self' https://*.firebaseapp.com https://*.google.com;"
           }
         ],
       },
